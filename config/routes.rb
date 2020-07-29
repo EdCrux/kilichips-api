57a45096ad07d782ca3f154b5ab25b43e5df227b
Eduardo <jecruxiz@gmail.com>
2020-07-29 17:34:45 -0500

    :sparkles: Add routing with api subdomain for the endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  
+  namespace :api, :path => "", :constraints => {:subdomain => "api"} do
+    namespace :v1 do
+      
+    end
+  end
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 end
