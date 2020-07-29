fa67320ac39ee287fdbf564b98f8d9169a7afd2e
Eduardo <jecruxiz@gmail.com>
2020-07-29 16:58:58 -0500

    :rotating_light: Removing linter warnings and set up a new CI

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,11 +1,11 @@
-ActiveAdmin.register_page "Dashboard" do
-  menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
+ActiveAdmin.register_page 'Dashboard' do
+  menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
-  content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content title: proc { I18n.t('active_admin.dashboard') } do
+    div class: 'blank_slate_container', id: 'dashboard_default_message' do
+      span class: 'blank_slate' do
+        span I18n.t('active_admin.dashboard_welcome.welcome')
+        small I18n.t('active_admin.dashboard_welcome.call_to_action')
       end
     end
 
@@ -13,7 +13,7 @@ ActiveAdmin.register_page "Dashboard" do
     #
     # columns do
     #   column do
-    #     panel "Recent Posts" do
+    #     panel 'Recent Posts' do
     #       ul do
     #         Post.recent(5).map do |post|
     #           li link_to(post.title, admin_post_path(post))
@@ -23,10 +23,10 @@ ActiveAdmin.register_page "Dashboard" do
     #   end
 
     #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
+    #     panel 'Info' do
+    #       para 'Welcome to ActiveAdmin.'
     #     end
     #   end
     # end
-  end # content
+  end
 end
