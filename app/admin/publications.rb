3c0076aad8b55267ee0edfc934a101c70a3292ec
Eduardo <jecruxiz@gmail.com>
2020-07-30 17:00:46 -0500

    :fire: Remove an instance of active text

--- app/admin/publications.rb
+++ app/admin/publications.rb
@@ -1,13 +1,8 @@
 ActiveAdmin.register Publication do
 
   # See permitted parameters documentation:
-  controller do
-    helper ActionText::Engine.helpers
-  end
-
   permit_params :title, :content, :category_publication_id, :admin_user_id
 
-  
   index do
     id_column
     column :title
